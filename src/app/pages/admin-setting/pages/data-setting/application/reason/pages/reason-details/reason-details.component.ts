77d5e634540fcf57b62a36b0ab9390804b88032b
nanthiya <nanthiya@pinepacific.net>
2025-09-02 15:19:08 +0700

    feat(reason): add initial ReasonDetails component with service integration

--- src/app/pages/admin-setting/pages/data-setting/application/reason/pages/reason-details/reason-details.component.ts
+++ src/app/pages/admin-setting/pages/data-setting/application/reason/pages/reason-details/reason-details.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { ReasonService } from '../../../../../../../../services/admin-setting/reason/reason.service';
 import { Subject, takeUntil } from 'rxjs';
+import { TablesComponent } from '../../../../../../../../shared/components/tables/tables.component';
+import { Columns } from '../../../../../../../../shared/interfaces/tables/column.interface';
 
 @Component({
   selector: 'app-reason-details',
@@ -10,6 +12,12 @@ import { Subject, takeUntil } from 'rxjs';
   styleUrl: './reason-details.component.scss'
 })
 export class ReasonDetailsComponent {
+  @ViewChild('categoryAcceptTable') categoryAcceptTable!: TablesComponent;
+  @ViewChild('categoryDeclineTable') categoryDeclineTable!: TablesComponent;
+  @ViewChild('categoryNoShowTable') categoryNoShowTable!: TablesComponent;
+  @ViewChild('categoryOnHoldTable') categoryOnHoldTable!: TablesComponent;
+  @ViewChild('categoryOnboardedTable') categoryOnboardedTable!: TablesComponent;
+
   filterButtons = [
     { label: 'Edit', key: 'edit', color: '#000000' },
     { label: 'Save', key: 'save', color: '#000055' },
@@ -23,6 +31,18 @@ export class ReasonDetailsComponent {
 
   private destroy$ = new Subject<void>();
 
+  categoryColumns: Columns = [
+    { header: 'No.', field: '__index', type: 'number', align: 'center', width: '7%' },
+    { header: 'Details', field: 'reasonText', type: 'text', width: '75%', wrapText: true },
+    { header: 'Action', field: 'textlink', type: 'textlink', align: 'center', width: '18%', textlinkActions: ['edit-inrow','delete'] },
+  ];
+
+  categoryAcceptRows: any[] = [];
+  categoryDeclineRows: any[] = [];
+  categoryNoShowRows: any[] = [];
+  categoryOnHoldRows: any[] = [];
+  categoryOnboarededRows: any[] = [];
+
   constructor(
     private route: ActivatedRoute,
     private reasonService: ReasonService,
@@ -34,11 +54,21 @@ export class ReasonDetailsComponent {
     this.route.queryParams
       .pipe(takeUntil(this.destroy$))
       .subscribe(params => {
-        this.processName = params['processName'] || '';
+        this.processName = params['processName'].split('-').join(' ') || '';
         this.processId = params['processId'] || 0;
 
         this.fetchRecruitmentStagesWithReasons();
       });
+
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.formDetails = this.fb.group({
+      processName: [
+        { value: this.processName, disabled: true }
+      ],
+    });
   }
 
   fetchRecruitmentStagesWithReasons() {
@@ -65,5 +95,25 @@ export class ReasonDetailsComponent {
 
   onSaveClicked() {
     console.log('Save button clicked');
+  }
+
+  onAddAcceptClicked() {
+    console.log('Add Accept button clicked');
+  }
+
+  onAddDeclineClicked() {
+    console.log('Add Decline button clicked');
+  }
+
+  onAddNoShowClicked() {
+    console.log('Add Decline button clicked');
+  }
+
+  onAddOnHoldClicked() {
+    console.log('Add Decline button clicked');
+  }
+
+  onAddOnboardedClicked() {
+    console.log('Add Decline button clicked');
   }
 }
