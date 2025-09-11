5078f21abe819367382a0dc1af08bd2c0be2d665
nanthiya <nanthiya@pinepacific.net>
2025-09-11 14:20:58 +0700

    feat(score): create Score & Score Details components and setup navigation path

---
+++ src/app/pages/admin-setting/pages/data-setting/application/score/pages/score-details/score-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-score-details',
+  templateUrl: './score-details.component.html',
+  styleUrl: './score-details.component.scss'
+})
+export class ScoreDetailsComponent {
+
+}
