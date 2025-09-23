0470e746d5a04d93de519905c0309432789f9468
nanthiya <nanthiya@pinepacific.net>
2025-09-23 10:43:21 +0700

    feat(applications,filter): add initial ApplicationForm UI and enhance dynamic button styles with back navigation support

--- src/app/pages/application/application-form/application-form.component.ts
+++ src/app/pages/application/application-form/application-form.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class ApplicationFormComponent {
 
+  filterButtons: { label: string; key: string; color: string }[] = [];
+  disabledKeys: string[] = [];
+
+  constructor(
+
+  ) { }
+
+  ngOnInit() {
+    this.filterButtons = [
+      { label: 'Print', key: 'print', color: '#0055FF' },
+    ];
+  }
+
+  onFilterButtonClick(key: string) {
+    switch (key) {
+      case 'print': this.onPrintClicked(); break;
+    }
+  }
+
+  onPrintClicked() {
+    console.log('Print clicked');
+  }
 }
