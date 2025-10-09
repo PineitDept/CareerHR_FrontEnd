67e8b490a1488cbd4660a609a15504f399e2aaf1
pannatorn <pannatorn@pinepacific.com>
2025-10-09 16:37:54 +0700

    fix : change path environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true, // Enable production mode (for deployment)
-  apiUrl: 'http://pineapi.pinepacific.com', // Actual backend URL for deployment
+  apiUrl: 'http://pineapi.pinepacific.com/api', // Actual backend URL for deployment
 };
